4be782b63cfdc9235ec3abf1f4326fe68a712171
Praveen <praveen183239@gmail.com>
2018-12-02 00:30:23 -0800

    COMMITING MED-HELP

---
+++ MED-HELP 3/app/src/main/java/com/example/chsai/med_help/MedNewsActivity.java
@@ -0,0 +1,13 @@
+package com.example.chsai.med_help;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MedNewsActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.med_news_layout);
+
+    }
+}
---
+++ MED-HELP 3/app/src/main/java/com/example/chsai/med_help/PurchaseHistory.java
@@ -0,0 +1,14 @@
+package com.example.chsai.med_help;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PurchaseHistory extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_layout);
+
+    }
+}
+
